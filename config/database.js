e602990d4c7cae08289e080a37c332488eb50b26
unknown <etiennepousse@live.fr>
2021-04-26 15:44:05 +0200

    added document root for node js plesk configuration : https://raoulkramer.de/deploy-run-host-strapi-on-plesk-obsidian-as-node-application/

--- config/database.js
+++ config/database.js
@@ -1,15 +1,28 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: env("DATABASE_CONNECTION_NAME"),
   connections: {
-    default: {
-      connector: 'bookshelf',
+    sqlite: {
+      connector: "bookshelf",
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: "sqlite",
+        filename: env("DATABASE_FILENAME", ".tmp/data.db"),
       },
       options: {
         useNullAsDefault: true,
       },
+    },
+    mysql: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST"),
+        port: env.int("DATABASE_PORT"),
+        database: env("DATABASE_NAME"),
+        username: env("DATABASE_USERNAME"),
+        password: env("DATABASE_PASSWORD"),
+        ssl: env.bool("DATABASE_SSL", false),
+      },
+      options: {},
     },
   },
 });
